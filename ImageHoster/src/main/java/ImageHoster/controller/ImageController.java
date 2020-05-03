d9349cacfd5f7f85ce58c5ab98e309de6b019694
venushaa <venushaa@hotmail.com>
2020-05-03 12:48:18 -0700

    fixed same tile error

--- ImageHoster/src/main/java/ImageHoster/controller/ImageController.java
+++ ImageHoster/src/main/java/ImageHoster/controller/ImageController.java
@@ -45,9 +45,11 @@ public class ImageController {
     //Also now you need to add the tags of an image in the Model type object
     //Here a list of tags is added in the Model type object
     //this list is then sent to 'images/image.html' file and the tags are displayed
-    @RequestMapping("/images/{title}")
-    public String showImage(@PathVariable("title") String title, Model model) {
-        Image image = imageService.getImageByTitle(title);
+
+    //adding imageID to make the image identified unique even though the titles are same {imageId}/{title}
+    @RequestMapping("/images/{imageId}/{title}")
+    public String showImage(@PathVariable("title") String title, @PathVariable("imageId") Integer id, Model model) {
+        Image image = imageService.getImageByTitleId(title, id);
         model.addAttribute("image", image);
         model.addAttribute("tags", image.getTags());
         return "images/image";
