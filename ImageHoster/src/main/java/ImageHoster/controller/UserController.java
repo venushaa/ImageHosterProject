2ea768c24322ca9b45149416d36ebf30b9d3c4fc
venushaa <venushaa@hotmail.com>
2020-05-03 13:33:05 -0700

    implemented password checker

--- ImageHoster/src/main/java/ImageHoster/controller/UserController.java
+++ ImageHoster/src/main/java/ImageHoster/controller/UserController.java
@@ -40,9 +40,26 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model) {
+        //check if the password is valid
+        if(passwordValidation(user.getPassword())) {
+            userService.registerUser(user);
+            return "redirect:/users/login";
+        }
+        else{
+            String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+            model.addAttribute("User", user);
+            model.addAttribute("passwordTypeError",error);
+            return "users/registration";
+        }
+    }
+
+    //apply password rules using RegEx
+    public boolean passwordValidation(String password) {
+        String strRegEx = "^(?=.*[0-9]).{3,}$";
+        if(password.matches( strRegEx ))
+            return true;
+        else return false;
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
